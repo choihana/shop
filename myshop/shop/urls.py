a4534a8f4cb86712bf1e1ffb568b25d93d66d9eb
Joey <hanazzang999@gmail.com>
2023-08-08 11:09:44 +0900

    list templates

--- myshop/shop/urls.py
+++ myshop/shop/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name='shop'
 urlpatterns = [
-    path('', views.product_list , name= 'product_list'),
-    path('<slug:category_slug>/', views.product_list , name='product_list_by_category'),
-    path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('', views.product_list, name='product_list'),
+    re_path(r'^(?P<category_slug>[-\w]+)/$', views.product_list, name='product_list_by_category'),
+    re_path(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.product_detail, name='product_detail'),
 ]
