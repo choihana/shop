be2919c5b41ae9a23127af8e797e22655c8dd316
Joey <hanazzang999@gmail.com>
2023-08-08 13:20:25 +0900

    장바구니 추가 함수

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -14,3 +14,21 @@ class Cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
 
         self.cart = cart
+
+    def add(self,product, quantity = 1 , override_quantity = False):
+        """
+        제품을 장바구니에 추가하거나 수량을 업데이트
+        """
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity':0,
+                                     'price': str(product.price)}
+        if override_quantity:
+            self.cart[product_id]['quantity'] = quantity
+
+        else:
+            self.cart[product_id]['quantity'] += quantity
+
+    def save(self):
+        # 세션이 '수정됨'으로 표시되도록 설정하여 저장되도록
+        self.session.modified = True
