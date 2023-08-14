60ecc2a88870a9e511d3d186e4687663cb2798e5
Joey <hanazzang999@gmail.com>
2023-08-14 15:47:05 +0900

    paid webhook add

---
+++ myshop/payment/webhooks.py
@@ -0,0 +1,38 @@
+import stripe
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from orders.models import Order
+
+
+@csrf_exempt #csrf 검증 수행하지 않도록하는 데코레이터 (strip과의 약속)
+def stripe_webhook(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload,
+            sig_header,
+            settings.STRIPE_WEBHOOK_SECRET
+        )
+    except ValueError as e:
+        # invalid payload
+        return HttpResponse(status = 400)
+    except stripe.error.SignatureVerificationError as e:
+        # invalid signature
+        return HttpResponse(status = 400)
+
+    # 결제 이벤트 체크
+    if event.type == 'checkout.session.completed':
+        session = event.data.object
+        if session.mode == 'payment' and session.payment_status == 'paid':
+            try:
+                order = Order.objects.get(id=session.client_reference_id)
+            except Order.DoesNotExist:
+                return HttpResponse(status = 404)
+            order.paid = True
+            order.save()
+    # success
+    return HttpResponse(status =200)
