34eb3164bae9b8de750771861fa1de7701afb97c
Joey <hanazzang999@gmail.com>
2023-08-16 14:09:48 +0900

    export to csv

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,4 +1,10 @@
+import csv
+import datetime
+
 from django.contrib import admin
+from django.http import HttpResponse
+from django.utils.safestring import mark_safe
+
 from .models import OrderItem, Order
 
 
@@ -7,9 +13,41 @@ class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
 
+# Order 객체를 인수로 받아 결제 URL이 포함된 HTML 링크를 반환
+def order_payment(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id} </a>'
+        return mark_safe(html)
+    return ''
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = f'attachmemt; filename = {opts.verbose_name}.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not \
+              field.many_to_many and not field.one_to_many]
+    writer.writerow([field.verbose_name for field in fields])
+
+    for obj in queryset:
+        data_row= []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+
+        writer.writerow(data_row)
+    return response
+export_to_csv.short_description = 'Export to CSV'
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id','first_name','last_name','email','address',
                     'postal_code','city','paid','created','updated']
     list_filter = ['paid','created','updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
