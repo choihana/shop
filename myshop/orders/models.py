47cc752e4bb67aefef41454cde0b9a445fb24941
Joey <hanazzang999@gmail.com>
2023-08-17 15:33:56 +0900

    order model에 coupon 연결

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -1,6 +1,8 @@
 from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from shop.models import Product
+from coupons.models import Coupon
 
 # Create your models here.
 
@@ -17,6 +19,10 @@ class Order(models.Model):
 
     # stripe 결제와 연결
     stripe_id = models.CharField(max_length=250, blank = True)
+
+    # coupon 연결
+    coupon = models.ForeignKey(Coupon, related_name='orders', null=True, blank=True, on_delete=models.SET_NULL)
+    discount = models.IntegerField(default = 0, validators=[MinValueValidator(0), MaxValueValidator(100)])
 
     class Meta:
         ordering = ['-created']
