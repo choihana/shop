406bb428a3763a14c25c1f3f2d9bddabb1935e5e
Joey <hanazzang999@gmail.com>
2023-08-17 14:09:01 +0900

    cart에 coupon 함수 정의

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from shop.models import Product
+from coupons.models import Coupon
 
 
 class Cart:
@@ -16,6 +17,31 @@ class Cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
 
         self.cart = cart
+
+        # 현재 세션에서 coupon_id 가져옴
+        self.coupon_id = self.session.get('coupon_id')
+
+        # 해당 값을 Cart 객체에 저장
+        @property
+        def coupon(self):
+            if self.coupon_id:
+                try:
+                    return Coupon.objects.get(id = self.coupon_id)
+                except Coupon.DeseNotExist:
+                    pass
+            return None
+
+        # 카트에 쿠폰이 포함된 경우 할인할 금액을 반환
+        def get_discount(self):
+            if self.coupon:
+                return (self.coupon.discount / Decimal(100)) \
+                        * self.get_total_price()
+            return Decimal(0)
+
+        # 전체 가격에서 할인가격 빼서 반환
+        def get_total_price_after_discount(self):
+            return self.get_total_price() - self.get_discount
+
 
     def add(self,product, quantity = 1 , override_quantity = False):
         """
