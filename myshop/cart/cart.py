f0080552205e155605e9a3b0f0a4b1f85ea00bc5
Joey <hanazzang999@gmail.com>
2023-08-17 15:23:57 +0900

    할인 적용 안되는 에러 수정

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from django.conf import settings
+from myshop import settings
 from shop.models import Product
 from coupons.models import Coupon
 
@@ -22,27 +22,6 @@ class Cart:
         self.coupon_id = self.session.get('coupon_id')
 
         # 해당 값을 Cart 객체에 저장
-        @property
-        def coupon(self):
-            if self.coupon_id:
-                try:
-                    return Coupon.objects.get(id = self.coupon_id)
-                except Coupon.DeseNotExist:
-                    pass
-            # 쿠폰 없는 경우 None 반환
-            return None
-
-        # 카트에 쿠폰이 포함된 경우 할인할 금액을 반환
-        def get_discount(self):
-            if self.coupon:
-                return (self.coupon.discount / Decimal(100)) \
-                        * self.get_total_price()
-            return Decimal(0)
-
-        # 전체 가격에서 할인가격 빼서 반환
-        def get_total_price_after_discount(self):
-            return self.get_total_price() - self.get_discount
-
 
     def add(self,product, quantity = 1 , override_quantity = False):
         """
@@ -106,4 +85,25 @@ class Cart:
         #세션에서 장바구니 제거
         del self.session[settings.CART_SESSION_ID]
         self.save()
+
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            try:
+                return Coupon.objects.get(id=self.coupon_id)
+            except Coupon.DeseNotExist:
+                pass
+        # 쿠폰 없는 경우 None 반환
+        return None
+
+    # 카트에 쿠폰이 포함된 경우 할인할 금액을 반환
+    def get_discount(self):
+        if self.coupon:
+            return (self.coupon.discount / Decimal(100)) \
+                        * self.get_total_price()
+        return Decimal(0)
+
+        # 전체 가격에서 할인가격 빼서 반환
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
 
--- myshop/coupons/views.py
+++ myshop/coupons/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.utils import timezone
 from django.views.decorators.http import require_POST
 
@@ -7,6 +7,7 @@ from coupons.models import Coupon
 
 
 # Create your views here.
+
 @require_POST
 def coupon_apply(request):
     now = timezone.now()
@@ -14,12 +15,11 @@ def coupon_apply(request):
     if form.is_valid():
         code = form.cleaned_data['code']
         try:
-            coupon = Coupon.objects.get(code__iexact =code,
-                                        valid_from__lte = now,
-                                        valid_to__gte = now,
+            coupon = Coupon.objects.get(code__iexact=code,
+                                        valid_from__lte=now,
+                                        valid_to__gte=now,
                                         active=True)
             request.session['coupon_id'] = coupon.id
-
         except Coupon.DoesNotExist:
             request.session['coupon_id'] = None
     return redirect('cart:cart_detail')
